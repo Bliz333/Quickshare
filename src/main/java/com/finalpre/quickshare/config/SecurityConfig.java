5b563d20d47111a36d93ae1d71a6b1c0c4eafcc0
Bliz333 <athesfly@gmail.com>
2026-04-10 02:45:17 -0700

    Allow access to share tools page

--- src/main/java/com/finalpre/quickshare/config/SecurityConfig.java
+++ src/main/java/com/finalpre/quickshare/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/index.html",
+                                "/share.html",
                                 "/login.html",
                                 "/register.html",
                                 "/netdisk.html",
